515283232520fc0977b35b651492a2d6c6e71bb5
VickyXT <vickyxt@163.com>
2017-11-27 21:20:55 +0800

    add netutil 解决键盘遮挡问题

--- app/src/main/java/com/vickyxt/onlinedorm/Login.java
+++ app/src/main/java/com/vickyxt/onlinedorm/Login.java
@@ -1,8 +1,35 @@
 package com.vickyxt.onlinedorm;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.vickyxt.util.NetUtil;
+
 /**
  * Created by VickyXT on 2017/11/27.
  */
 
-public class Login {
+public class Login extends Activity {
+
+    private ImageView LoginBtn;
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.login);
+
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE){
+            Log.d("myWeather","网络ok");
+            Toast.makeText(Login.this,"网络ok！",Toast.LENGTH_LONG).show();
+        }
+        else{
+            Log.d("myWeather","网络挂了");
+            Toast.makeText(Login.this,"网络挂了",Toast.LENGTH_LONG).show();
+        }
+
+    }
 }
