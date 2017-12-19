ecca6b0e68665907f98e5fedf1a6d6c5320c1b47
VickyXT <vickyxt@163.com>
2017-12-19 18:49:19 +0800

    edit user_info

--- app/src/main/java/com/vickyxt/onlinedorm/SelectDorm.java
+++ app/src/main/java/com/vickyxt/onlinedorm/SelectDorm.java
@@ -1,8 +1,46 @@
 package com.vickyxt.onlinedorm;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import me.majiajie.pagerbottomtabstrip.NavigationController;
+import me.majiajie.pagerbottomtabstrip.PageNavigationView;
+import me.majiajie.pagerbottomtabstrip.item.BaseTabItem;
+
+
 /**
  * Created by VickyXT on 2017/11/29.
  */
 
-public class SelectDorm {
+public class SelectDorm extends Activity implements View.OnClickListener {
+
+
+    void initView(){
+        setContentView(R.layout.user_info);
+        PageNavigationView tab = (PageNavigationView) findViewById(R.id.tab);
+
+        NavigationController navigationController = tab.material()
+                .addItem(android.R.drawable.ic_menu_edit, "主页")
+                .addItem(android.R.drawable.ic_menu_info_details, "我的")
+                .build();
+    }
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+
+        initView();
+    }
+
+    @Override
+    public void onClick(View view){
+
+//        if (view.getId() == R.id.login_btn){
+//            Intent i = new Intent(this, SelectDorm.class);
+//            startActivityForResult(i,1);
+//        }
+    }
+
+
 }
