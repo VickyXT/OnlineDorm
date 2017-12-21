76daa0a022fa6509306f20d2eeb6d52f8a1373ad
VickyXT <vickyxt@163.com>
2017-12-21 14:42:23 +0800

    add result

--- app/src/main/java/com/vickyxt/onlinedorm/Result.java
+++ app/src/main/java/com/vickyxt/onlinedorm/Result.java
@@ -4,26 +4,21 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 /**
  * Created by VickyXT on 2017/12/21.
  */
 
-public class SingleChoose extends Activity implements View.OnClickListener {
+public class Result extends Activity implements View.OnClickListener {
 
     private ImageView backBtn;
-    private Button submitBtn;
 
     void initView(){
-        setContentView(R.layout.single_choose);
+        setContentView(R.layout.result);
 
         backBtn = (ImageView)findViewById(R.id.back_btn);
         backBtn.setOnClickListener(this);
-
-        submitBtn = (Button)findViewById(R.id.submit_btn);
-        submitBtn.setOnClickListener(this);
     }
 
     @Override
@@ -36,12 +31,7 @@ public class SingleChoose extends Activity implements View.OnClickListener {
     public void onClick(View v) {
 
         if(v.getId() == R.id.back_btn){
-            Intent i = new Intent(this, SelectDorm.class);
-            startActivityForResult(i,1);
-        }
-
-        if(v.getId() == R.id.submit_btn){
-            Intent i = new Intent(this, Result.class);
+            Intent i = new Intent(this, UserInfo.class);
             startActivityForResult(i,1);
         }
 
